e45411662e746dfc015dd08fa49d978ac71492ed
ycode2023 <133668282+ycode2023@users.noreply.github.com>
2023-07-28 19:28:24 +0100

    weather app jsx and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import './App.css';
+import React from "react";
 import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-        <Weather />
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://yaldakesh.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Yalda Keshavarzi
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/ycode2023"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://yk-reactweatherapp.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
+    </div>
   );
 }
