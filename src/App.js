7a13d9b1b3d5ed0bd22fa085382c7dba02090c89
ycode2023 <133668282+ycode2023@users.noreply.github.com>
2023-07-28 18:52:17 +0100

    cleaned up files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Weather from "./Weather";
-import axios from 'axios';
 
 export default function App() {
   return (
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-import Loader from "react-loader-spinner";
 
 
 export default function Weather () {
