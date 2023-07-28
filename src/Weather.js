b266195aa421fa40c6c62873a877018bd5ef82c8
ycode2023 <133668282+ycode2023@users.noreply.github.com>
2023-07-28 18:27:37 +0100

    set up React app

---
+++ src/Weather.js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+
+export default function Weather () {
+    return (
+      <div>
+        <h2>Hello</h2>
+      </div>
+    ); 
+}
